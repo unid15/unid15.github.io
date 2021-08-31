59c713c872840f36916a0f9b314ffeeb732d26fe
golmgnik <golmgnik@fit.cvut.cz>
2021-08-31 12:35:18 +0200

    Add cookie game

---
+++ index.js
@@ -0,0 +1,36 @@
+let cookieNumber = 0;
+let notificationField = document.querySelector('#notification');
+
+const BC_COOKIE_AMOUNT = 'cookieAmount';
+
+let loaded = getBrowserCookie(BC_COOKIE_AMOUNT);
+cookieNumber = Number.isFinite(loaded) ? loaded : 0;
+updateCookieTracker();
+
+function notify(message) {
+	notificationField.innerHTML = `<p>${message}</p>`;
+}
+
+function updateCookieTracker() {
+	notify(`You now have: ${cookieNumber} cookie${cookieNumber==1 ? '' : 's'}`);
+}
+
+function cookieButton() {
+	cookieNumber++;
+	saveBrowserCookie(BC_COOKIE_AMOUNT, cookieNumber);
+	updateCookieTracker();
+}
+
+function saveBrowserCookie(cookieName, cookieValue) {
+	const date = new Date();
+	const EXPIRATION_DAYS = 10000;
+	date.setTime(date.getTime() + (EXPIRATION_DAYS*1000*60*60*24));
+	document.cookie = `${cookieName}=${cookieValue};SameSite=Strict;expires=${date.toUTCString()};path=/`;
+}
+
+function getBrowserCookie(cookieName) {
+	let name = cookieName + "=";
+	let decodedBrowserCookie = decodeURIComponent(document.cookie);
+	let ca = decodedBrowserCookie.split(';');
+	console.log(ca);
+}
